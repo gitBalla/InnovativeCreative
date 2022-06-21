823241b94a72c0c3622edefb404d41fbfafb781d
Nicholasdluong <nicholasdluong@gmail.com>
2022-06-22 01:34:28 +1000

    Created ImagePicker

---
+++ InnovativeCreative/ImagePicker.swift
@@ -0,0 +1,55 @@
+//
+//  ImagePicker.swift
+//  InnovativeCreative
+//
+//  Created by Nicholas on 22/6/22.
+//
+
+import SwiftUI
+import UIKit
+
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    @Binding var uiImage: UIImage?
+    @Binding var isPresenting: Bool
+    @Binding var sourceType: UIImagePickerController.SourceType
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = context.coordinator
+        return imagePicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+    }
+    
+    typealias UIViewControllerType = UIImagePickerController
+        
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
+        let parent: ImagePicker
+                
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            parent.uiImage = info[.originalImage] as? UIImage
+            parent.isPresenting = false
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.isPresenting = false
+        }
+        
+        init(_ imagePicker: ImagePicker) {
+            self.parent = imagePicker
+        }
+        
+    }
+    
+}
