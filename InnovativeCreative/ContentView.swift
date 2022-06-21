ceec97a290acd16abf1f6745834ce521c85b27e8
johnballauts <john.a.balla@student.uts.edu.au>
2022-06-21 14:42:27 +1000

    Initial Commit

---
+++ InnovativeCreative/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InnovativeCreative
+//
+//  Created by John Balla on 21/6/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InnovativeCreative/InnovativeCreativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnovativeCreativeApp.swift
+//  InnovativeCreative
+//
+//  Created by John Balla on 21/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct InnovativeCreativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
